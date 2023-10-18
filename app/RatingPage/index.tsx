fc705a092c2e156513996594eca137f375695753
maiconlara <maiconluan.lara@yahoo.com.br>
2023-10-18 08:59:57 -0300

    static rating data

--- app/RatingPage/index.tsx
+++ app/RatingPage/index.tsx
@@ -1,5 +1,26 @@
 
-
+const Rating = [
+    {
+      id: 1,
+      totalReviews: 10,
+    },
+    {
+      id: 2,
+      totalReviews: 30,
+    },
+    {
+      id: 3,
+      totalReviews: 40,
+    },
+    {
+      id: 4,
+      totalReviews: 60,
+    },
+    {
+      id: 5,
+      totalReviews: 80,
+    },
+  ]
 
 const RatingPage = () => {
 
