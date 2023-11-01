60490126bfa877edb0c656d16336221986f6a8ab
maiconlara <maiconluan.lara@yahoo.com.br>
2023-11-01 11:25:50 -0300

    refactor: swapping  home with login

---
+++ app/Home/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FlatList, ScrollView, View, VStack } from "native-base";
+import { coffeeShops, Drinks } from "../../src/mocks/index";
+import { handleNavigate, goBack } from "../../src/utils/handleNavigate";
+
+import HeaderHome from "../../src/components/HeaderHome";
+import { BannerHome } from "../../src/components/BannerHome";
+import CoffeeShopCard from "../../src/components/CoffeeShopCard";
+import SwiperTitle from "../../src/components/SwiperTitle";
+import CoffeeCard from "../../src/components/CoffeeCard";
+
+import { CoffeeDrinks } from "../../src/interfaces/coffeeDrinks";
+import { CoffeeShops } from "../../src/interfaces/coffeeShops";
+
+export default function Home() {
+  return (
+    <VStack safeAreaBottom>
+      <HeaderHome />
+      <ScrollView h={"full"}>
+        <BannerHome />
+        <SwiperTitle title="Cafeterias por perto" />
+        <FlatList
+          data={coffeeShops}
+          renderItem={({ item }) => (
+            <CoffeeShopCard data={item} navigate={() => handleNavigate("CoffeeShop")} />
+          )}
+          keyExtractor={(item: CoffeeShops) => item.id.toString()}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          paddingLeft={8}
+          paddingTop={3}
+        />
+        <SwiperTitle title="Mais pedidos" />
+        <View
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          px={8}
+          flex={1}
+          paddingBottom={200}
+        >
+          {Drinks.map((item: CoffeeDrinks) => (
+            <CoffeeCard key={item.id} data={item} navigate={() => handleNavigate("Order")} />
+          ))}
+        </View>
+      </ScrollView>
+    </VStack>
+  );
+}
