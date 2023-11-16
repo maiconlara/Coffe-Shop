239bb08b965324857923032187a1a4c2e30b4855
Maicon <maiconluan.lara@yahoo.com.br>
2023-11-15 23:38:24 -0300

    feat: login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,40 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+import logo from "../assets/adaptive-icon.png";
 
 const App = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Hemllo</Text>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Image source={logo} style={{ height: 280, width: 280 }} />
+        <Text style={styles.title}>Coffee Shop</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          keyboardType="number-pad"
+          style={styles.input}
+          onChangeText={() => {}}
+        ></TextInput>
+        <TextInput
+          placeholder="Senha"
+          secureTextEntry={true}
+          style={styles.input}
+          onChangeText={() => {}}
+        ></TextInput>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={() => {
+            router.push("Home");
+          }}
+        >
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
