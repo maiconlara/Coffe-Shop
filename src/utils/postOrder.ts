6ad7b97fe379156340bdaa6be2f294daebab2c0e
Maicon <maiconluan.lara@yahoo.com.br>
2023-11-22 19:08:22 -0300

    refactor: changes in postOder types

--- src/utils/postOrder.ts
+++ src/utils/postOrder.ts
@@ -1,10 +1,23 @@
 import { api } from "../lib/api";
+import { Topping } from "./getToppings";
+import { EssenceType } from "./getTypesOfEssence";
+import { MilkType } from "./getTypesOfMilk";
 
-export interface Order {
-  pedido_id: number;
-  user_id: number;
-  shop_id: number;
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  img: string;
+  typesOfMilk: MilkType;
+  typesOfEssence: EssenceType;
+  topping: Topping;
+}
+
+interface Order {
+  product: Product[];
+  user_id: string;
   valor_total: number;
+  id_cafeteria: number;
   data_compra: Date;
 }
 
